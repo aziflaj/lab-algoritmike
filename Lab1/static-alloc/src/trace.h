505e62bf4f1daee49b6338401298c109491c2528
aziflaj <aldoziflaj95@gmail.com>
2015-04-03 12:25:39 -0700

    JavaDoc to trace.h for the static algorithm

--- Lab1/static-alloc/src/trace.h
+++ Lab1/static-alloc/src/trace.h
@@ -10,14 +10,58 @@ typedef struct {
 	float value[MAX_RECORDS];	/* The value measured */
 } Trace;
 
+
+/**
+ * This function simulates a Trace instance based in the FitzHugh - Nagumo equation
+ *
+ * @param tmax The finish time of the simulation
+ * @param dt The difference of time between simulations
+ * @param params The alpha, delta, epsilon array of parameters to simulate
+ * @param uneTrace The Trace instance to be simulated
+ *
+ * @author: Aldo Ziflaj
+ */
 void simulate(int tmax, float dt, float params[], Trace *uneTrace);
 
+
+/**
+ * This function prints a Trace instance 
+ *
+ * @author: Aldo Ziflaj
+ */
 void printTrace(Trace uneTrace);
 
+
+/**
+ * This function saves a Trace instance in a file
+ *
+ * @param fileTrace The name of the file where to be saved
+ * @param uneTrace The Trace instance to be saved
+ *
+ * @author: Aldo Ziflaj
+ */
 void saveTraceBin(char* fileTrace, Trace uneTrace);
 
+
+/**
+ * This function reads a Trace instance from a file
+ *
+ * @param fileTrace The name of the file where to read
+ * @param uneTrace The Trace instance to read
+ *
+ * @author: Aldo Ziflaj
+ */
 void readTrace(char* fileTrace, Trace* uneTrace);
 
+
+/**
+ * This function calculates the error between two Trace instances
+ *
+ * @param simTrace The simulated Trace instance
+ * @param realTrace The real Trace instance, measured
+ *
+ * @author: Aldo Ziflaj
+ */
 float errorTrace(Trace simTrace, Trace realTrace);
 
 
