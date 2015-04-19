299f5a4451352eae1ab601f201548242fb0d9781
aziflaj <aldoziflaj95@gmail.com>
2015-04-19 11:56:13 -0700

    added debug function helper

--- Lab2/libs/utils.h
+++ Lab2/libs/utils.h
@@ -3,17 +3,37 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#include <stdarg.h>
 
 char* subString(char* str, int from, int to) {
 	char *newStr;
-	int len = to - from;
+	
+	printf("params: %d, %d\n", from,to);
+	if (from < 0)  from = 0;;
+
+	if (to > (strlen(str) - 1)) 
+		to = strlen(str);
+
+	printf("params changed: %d, %d\n", from,to);
+
+	int len = to - from ;
 	newStr = (char*) malloc(sizeof(char) * len);
 
-	for (int i = 0; i <= len; i++) {
+	for (int i = 0; i < len; i++) {
 		newStr[i] = str[from+i];
 	}
 
 	return newStr;
+}
+
+void debug(const char *format, ...) {
+	va_list args;
+	printf("DEBUG: ");
+	va_start(args, format);
+	vprintf(format, args);
+	va_end(args);
+	printf("\n");
 }
 
 #endif /* _UTILS_H_ */
