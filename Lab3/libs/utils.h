9eb184e52cd4601aa9d3231ea75599dd40f1a689
Aldo Ziflaj <aldoziflaj95@gmail.com>
2015-05-18 08:48:16 +0200

    initial for Lab3

---
+++ Lab3/libs/utils.h
@@ -0,0 +1,23 @@
+#ifndef _UTILS_H_
+#define _UTILS_H_
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdarg.h>
+
+void debug(const char *format, ...) {
+	va_list args;
+	printf("DEBUG: ");
+	va_start(args, format);
+	vprintf(format, args);
+	va_end(args);
+	printf("\n");
+}
+
+void printString(char *str, int len) {
+	for (int i=0; i<len;i++) {
+		printf("%c", str[i]);
+	}
+}
+
+#endif /* _UTILS_H_ */
